d0f2915412046da28d16858234ca54736c5dffd3
yuvals38 <yuval.s@scodix.com>
2020-03-05 09:46:20 +0200

    core SignalR R&D
    .net core with signalR research

---
+++ NET-core-SignalR/console-client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalR.Samples.ConsoleClient
+{
+    class Program
+    {
+        private static HubConnection _connection;
+        public static async Task Main(string[] args)
+        {
+
+            await StartConnection();
+            _connection.On<string, string>("broadcastMessage", (name, message) =>
+            {
+                Console.WriteLine($"{name} said: {message}");
+            });
+
+            _connection.On("streamStarted", async () =>
+            {
+                await StartStreaming();
+            });
+
+            Console.ReadLine();
+            await Dispose();
+        }
+
+        public async static Task StartStreaming()
+        {
+            var channel = await _connection.StreamAsChannelAsync<string>("StartStreaming", CancellationToken.None);
+            while (await channel.WaitToReadAsync())
+            {
+                while (channel.TryRead(out string message))
+                {
+                    Console.WriteLine($"Message received: {message}");
+                }
+            }
+        }
+
+        public static async Task StartConnection()
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/demo")
+                //to enable message pack for the .NET client, uncomment the following line
+                //.AddMessagePackProtocol()
+                .Build();
+
+            await _connection.StartAsync();
+        }
+
+        public static async Task Dispose()
+        {
+            await _connection.DisposeAsync();
+        }
+    }
+}
---
+++ NET-core-SignalR/simple-chat/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SignalR.Samples.SimpleChat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR()
+                .AddMessagePackProtocol();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseFileServer();
+            app.UseEndpoints(routes =>
+            {
+                routes.MapHub<DemoHub>("/demo");
+            });
+        }
+    }
+}
